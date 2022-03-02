375208db806816d29adb12a32839da02a52cdc9f
Jordan Kueh <jkueh@users.noreply.github.com>
2022-03-02 17:38:53 +1100

    Tweaking webhook content formatting

--- main.js
+++ main.js
@@ -132,7 +132,7 @@ if (exitWithError) {
 
       console.log("Sending webhook");
       // await webhook.send(`<@168004824628068352> BVMS Appointments:\n\`\`\`${messageArr.join("\n")}\`\`\``);
-      await webhook.send(`<@168004824628068352>\n` + "```" + `${messageArr.join("\n")}` + "```", [
+      await webhook.send(`<@168004824628068352>`, [
         {
           title: `Stock Levels`,
           fields: webhookFields,
